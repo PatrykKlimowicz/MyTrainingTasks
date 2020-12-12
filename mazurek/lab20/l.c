d064dd04d2c734cf8be26448436fce394caa9291
Patryk Klimowicz <klimczynsky@gmail.com>
2020-12-12 13:09:39 -0500

    lab20 is done

--- mazurek/lab20/l.c
+++ mazurek/lab20/l.c
@@ -1,39 +1,58 @@
 #include <stdio.h>
 #include <stdlib.h>
- 
-float systemp, millideg;
-FILE *thermal;
-int n;
 
-get_temp(){
- 
-        thermal = fopen("/sys/class/hwmon/hwmon3/temp1_input","r");
-        n = fscanf(thermal,"%f",&millideg);
-        fclose(thermal);
-        systemp = millideg / 1000;
- 
-        return printf("CPU temperature is %f degrees C\n",systemp);
+float get_cpu_temperature() {
+        FILE *f = fopen("/sys/class/hwmon/hwmon4/temp1_input", "r");
+
+	float milideg;
+	fscanf(f, "%f", &milideg);
+        fclose(f);
+        float cpu_temp = milideg / 1000;
+
+	return cpu_temp;
 }
- 
-int main()
-{
-	for(int i = 0; i < 20; i++)
+
+int get_fan_1_status() {
+        FILE *f = fopen("/sys/class/hwmon/hwmon4/fan1_input", "r");
+
+        int status;
+        fscanf(f, "%d", &status);
+        fclose(f);
+
+        return status;
+}
+
+int get_fan_2_status() {
+        FILE *f = fopen("/sys/class/hwmon/hwmon4/fan2_input", "r");
+
+        int status;
+        fscanf(f, "%d", &status);
+        fclose(f);
+
+        return status;
+}
+
+
+int main() {
+	printf("CPU temp: %f\n", get_cpu_temperature());
+        printf("Fan1 status: %d rpm\n", get_fan_1_status());
+        printf("Fan2 status: %d rpm\n", get_fan_2_status());
+	for(int j = 0; j < 20; j++)
     	{
-        	unsigned long long next = 0, c = 0, first = 0, second = 1;
- 
-        	for(c = 0; c < 2000000000; c++)
-        	{
-            		if(c<=1)
-                		next = c;
-            		else
-            		{
-                		next = first + second;
-                		first = second;
-                		second = next;
-            		}
-        	}
- 
-        printf("obliczono: %lld\n\n", next);
-	get_temp();
+		double PI = 4;
+		int flag = 0;
+		for (unsigned long long i = 3; i < 2209900000; i+= 2) {
+			if (flag)
+				PI += 4.0 / i;
+			else
+				PI -= 4.0 / i;
+
+			flag = !flag;
+		}
+
+        	printf("Gregory Lebnitz PI: %0.30f\n\n", PI);
+		printf("CPU temp: %f\n", get_cpu_temperature());
+		printf("Fan1 status: %d rpm\n", get_fan_1_status());
+		printf("Fan2 status: %d rpm\n", get_fan_2_status());
 	}
 }
